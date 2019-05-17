ef9fc144d97f9d4f43e0f069c1631c043a740cdf
Роман Аверкин <romaaverkin@gmail.com>
2019-05-17 15:22:39 +0700

    initial Commit

---
+++ app/src/main/java/romaaverkingmail/com/playingaudio/MainActivity.java
@@ -0,0 +1,61 @@
+package romaaverkingmail.com.playingaudio;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    Button playButton;
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxValue = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setMax(maxValue);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.stuff);
+
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    playButton.setText("play");
+                } else {
+                    mediaPlayer.start();
+                    playButton.setText("play");
+                }
+            }
+        });
+    }
+}
